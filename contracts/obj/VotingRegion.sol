24f0aca025ae60de51adcf5ebdc316e883fbeda6
Benjamin Kok <benjaminkok@u.nus.edu>
2022-03-12 21:31:53 +0800

    Update VotingRegion.sol

--- contracts/obj/VotingRegion.sol
+++ contracts/obj/VotingRegion.sol
@@ -3,13 +3,7 @@ pragma solidity ^0.8.0;
 contract VotingRegions {
     address[] eligibleReferenceContracts; // eligible contracts that can modify the voting region, not used yet
 
-    modifier adminOnly {
-        require (isAdministrator(msg.sender), "Error, admin only");
-        _;
-    }
-
     constructor() {
-        administrators.push(msg.sender);
     }
 
     function getEligibleReferenceContracts() public view returns (address[] memory) {
